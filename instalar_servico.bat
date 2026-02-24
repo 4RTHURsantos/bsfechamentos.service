41b8dd8121c54ddc66afcdc2879f99a99ddfd9d3
Arthur Santos <99753279+4RTHURsantos@users.noreply.github.com>
2026-02-24 17:43:43 -0300

    fix: install .bat

--- instalar_servico.bat
+++ instalar_servico.bat
@@ -1,6 +1,8 @@
 @echo off
 
-==== INSTALANDO SERVIÇO BSFechamentos ====
+echo ==== INSTALANDO SERVIÇO BSFechamentos ====
+echo Esse .bat irá instalar o serviço BSFechamentos, para isso é necessário ter o Node.js instalado na máquina.
+pause
 
 echo Parando serviços semelhantes....
 net stop BSFechamentos
@@ -13,3 +15,4 @@ npm run build
 
 echo Instalando serviço BSFechamentos...
 npm run start
+pause
